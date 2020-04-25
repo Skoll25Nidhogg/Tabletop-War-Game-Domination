ba4a29945f98fbeca1cc50ded8f0e418b6924d75
Daniel <dosken88@gmail.com>
2020-04-24 20:54:03 -0500

    Un pixel de puch pls

---
+++ Assets/Assets/scripts/CharacterSelect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour
+{
+    int PlayerContador = 0;
+    public int CountPlus(int PlayerContador)
+    {
+        if (PlayerContador < 4)
+        {
+            PlayerContador++;
+        }
+
+        return (PlayerContador);
+    }
+
+    public int CountMinus(int PlayerContador)
+    {
+        if (PlayerContador > 0)
+        {
+            PlayerContador--;
+        }
+
+        return (PlayerContador);
+    }
+
+    public void PlayerCount()
+    {
+
+    }
+
+    public void CreacionJugador()
+    {
+
+    }
+}
